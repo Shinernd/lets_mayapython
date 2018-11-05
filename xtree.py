0cbdde3b6870d1aeacf7044347a076066d2adb61
Hwi <40123069+Shinernd@users.noreply.github.com>
2018-11-05 22:53:18 +0900

    Create xtree.py

---
+++ xtree.py
@@ -0,0 +1,33 @@
+import pymel.core as pm
+import random
+
+myPyramids = []
+
+#make pyramids
+for n in range (386):
+    myPyramids.append(pm.polyPyramid())
+            
+count = 0
+for x in range (10):
+    yran = 10 - x
+    for y in range (yran):
+        zran = 10 - y
+        for z in range (zran):
+            rg = int(random.random()*10)%2 #red or green?
+            pm.select(myPyramids[count])
+            pm.move(0.1*x*x, y, 0.1*z*z) #curve
+            pm.scale(1, 1, 1)
+            if (rg): #green
+                pm.polyColorPerVertex(colorRGB=[0, 1-random.random(), 0], colorDisplayOption=True)
+            else: #red
+                pm.polyColorPerVertex(colorRGB=[1-random.random(), 0, 0], colorDisplayOption=True)
+            pm.rotate(x*10.0, y*10.0, z*10.0)
+            count = count + 1
+
+#a star
+pm.select(myPyramids[count])
+print(count)
+pm.move(0, 10.1, 0)
+pm.scale(1,1,1)
+pm.rotate(0, 90, 0)
+pm.polyColorPerVertex(colorRGB=[1,1,0], colorDisplayOption=True)
