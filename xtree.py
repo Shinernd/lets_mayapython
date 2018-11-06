2d98850cba145a38a61e735257a349a5001ea29a
Hwi <40123069+Shinernd@users.noreply.github.com>
2018-11-06 20:03:42 +0900

    Update xtree.py

--- xtree.py
+++ xtree.py
@@ -13,20 +13,19 @@ for x in range (10):
     for y in range (yran):
         zran = 10 - y
         for z in range (zran):
-            rg = int(random.random()*10)%2 #red or green?
             pm.select(myPyramids[count])
             pm.move(0.1*x*x, y, 0.1*z*z) #curve
             pm.scale(1, 1, 1)
-            if (rg): #green
-                pm.polyColorPerVertex(colorRGB=[0, 1-random.random(), 0], colorDisplayOption=True)
-            else: #red
+            rg = int(random.random()*10)%2 #red or green?
+            if (rg): #red
                 pm.polyColorPerVertex(colorRGB=[1-random.random(), 0, 0], colorDisplayOption=True)
+            else: #green
+                pm.polyColorPerVertex(colorRGB=[0, 1-random.random(), 0], colorDisplayOption=True)
             pm.rotate(x*10.0, y*10.0, z*10.0)
             count = count + 1
 
 #a star
 pm.select(myPyramids[count])
-print(count)
 pm.move(0, 10.1, 0)
 pm.scale(1,1,1)
 pm.rotate(0, 90, 0)
