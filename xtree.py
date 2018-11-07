ad3f63f26243adf0a6acb103ef486b0a48df373a
Hwi <40123069+Shinernd@users.noreply.github.com>
2018-11-07 10:18:20 +0900

    Mirroring
    pm.polyMirrorFace
    'direction' flag specifies the mirror direction.
    'mergeMode' flag specifies the behaviour of the mirror with respect to the border edges. Valid values are 0-5, corresponding to +X, -X, +Y, -Y, +Z, -Z direction respectively. If the mode is 0, the border edges will not be merged (co-incident vertices will be present). If the mode is 1, the border vertices/edges will be merged If the mode is 2, the border edges will be extruded and connected.

--- xtree.py
+++ xtree.py
@@ -17,11 +17,12 @@ for x in range (10):
             pm.move(0.1*x*x, y, 0.1*z*z) #curve
             pm.scale(1, 1, 1)
             rg = int(random.random()*10)%2 #red or green?
-            if (rg): #red
-                pm.polyColorPerVertex(colorRGB=[1-random.random(), 0, 0], colorDisplayOption=True)
-            else: #green
+            if (rg): #green
                 pm.polyColorPerVertex(colorRGB=[0, 1-random.random(), 0], colorDisplayOption=True)
+            else: #red
+                pm.polyColorPerVertex(colorRGB=[1-random.random(), 0, 0], colorDisplayOption=True)
             pm.rotate(x*10.0, y*10.0, z*10.0)
+            pm.polyMirrorFace(direction=0, mergeMode=1)
             count = count + 1
 
 #a star
