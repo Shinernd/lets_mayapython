2db49d98e6440e4bcc231c10e20d76e0ecfaec39
Hwi <40123069+Shinernd@users.noreply.github.com>
2018-11-07 10:53:54 +0900

    Complete

--- xtree.py
+++ xtree.py
@@ -4,7 +4,7 @@ import random
 myPyramids = []
 
 #make pyramids
-for n in range (386):
+for n in range (387):
     myPyramids.append(pm.polyPyramid())
             
 count = 0
@@ -23,11 +23,19 @@ for x in range (10):
                 pm.polyColorPerVertex(colorRGB=[1-random.random(), 0, 0], colorDisplayOption=True)
             pm.rotate(x*10.0, y*10.0, z*10.0)
             pm.polyMirrorFace(direction=0, mergeMode=1)
+            pm.polyMirrorFace(direction=4, mergeMode=1)
             count = count + 1
 
-#a star
+#star 1
 pm.select(myPyramids[count])
-pm.move(0, 10.1, 0)
+pm.move(0, 10.1, 0.2)
 pm.scale(1,1,1)
 pm.rotate(0, 90, 0)
+pm.polyColorPerVertex(colorRGB=[1,1,0], colorDisplayOption=True)
+count = count + 1
+
+#star 2
+pm.select(myPyramids[count])
+pm.move(0, 10.1, -0.2)
+pm.rotate(45, 270, 0)
 pm.polyColorPerVertex(colorRGB=[1,1,0], colorDisplayOption=True)
