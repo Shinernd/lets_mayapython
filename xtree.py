325ef52c516d5488dcab60e033d2a983d3d52b2e
Hwi <40123069+Shinernd@users.noreply.github.com>
2018-11-07 13:28:37 +0900

    Tiny Leaves More

--- xtree.py
+++ xtree.py
@@ -4,18 +4,18 @@ import random
 myPyramids = []
 
 #make pyramids
-for n in range (387):
+for n in range (2872):
     myPyramids.append(pm.polyPyramid())
-            
+      
 count = 0
-for x in range (10):
-    yran = 10 - x
+for x in range (20):
+    yran = 20 - x
     for y in range (yran):
-        zran = 10 - y
+        zran = 20 - y
         for z in range (zran):
             pm.select(myPyramids[count])
-            pm.move(0.1*x*x, y, 0.1*z*z) #curve
-            pm.scale(1, 1, 1)
+            pm.move(0.05*x*x, y, 0.05*z*z) #curve
+            pm.scale(0.5, 0.5, 0.5)
             rg = int(random.random()*10)%2 #red or green?
             if (rg): #green
                 pm.polyColorPerVertex(colorRGB=[0, 1-random.random(), 0], colorDisplayOption=True)
@@ -28,14 +28,15 @@ for x in range (10):
 
 #star 1
 pm.select(myPyramids[count])
-pm.move(0, 10.1, 0.2)
-pm.scale(1,1,1)
+pm.move(0, 20.1, 0.2)
+pm.scale(1, 1, 1)
 pm.rotate(0, 90, 0)
 pm.polyColorPerVertex(colorRGB=[1,1,0], colorDisplayOption=True)
 count = count + 1
 
 #star 2
 pm.select(myPyramids[count])
-pm.move(0, 10.1, -0.2)
+pm.move(0, 20.1, -0.2)
+pm.scale(1, 1, 1)
 pm.rotate(45, 270, 0)
 pm.polyColorPerVertex(colorRGB=[1,1,0], colorDisplayOption=True)
